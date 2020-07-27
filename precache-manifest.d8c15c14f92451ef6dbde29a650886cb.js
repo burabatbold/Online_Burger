e3a58ec9d88711782d763b2d9be8e4181720320d
bura <burabatbold2@gmail.com>
2020-07-27 17:48:48 +0800

    Updates

---
+++ precache-manifest.d8c15c14f92451ef6dbde29a650886cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d55964e14c753d03146c289034a2ba",
+    "url": "/Online_Burger/index.html"
+  },
+  {
+    "revision": "e08dcf10ca6087666ce1",
+    "url": "/Online_Burger/static/css/main.2ddfc18a.chunk.css"
+  },
+  {
+    "revision": "a2563613edcdd94f82fe",
+    "url": "/Online_Burger/static/js/2.60f35ac1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Online_Burger/static/js/2.60f35ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e08dcf10ca6087666ce1",
+    "url": "/Online_Burger/static/js/main.d7cc647e.chunk.js"
+  },
+  {
+    "revision": "a2b79973c3181c39b6c5",
+    "url": "/Online_Burger/static/js/runtime-main.847ac0d0.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Online_Burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
